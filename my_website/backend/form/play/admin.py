ff857f548f6fdaebc9658161770dcd418633f6d3
Madina <69770324+matonyn@users.noreply.github.com>
2023-10-19 07:46:20 +0600

    Delete my_website/backend/form directory

--- my_website/backend/form/play/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Ref, resume
-# Register your models here.
-
-class imageAdmin(admin.ModelAdmin):
-    list_display = ["name", "surname", "image"] 
-
-admin.site.register(Ref, imageAdmin)
-admin.site.register(resume)
